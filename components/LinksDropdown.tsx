680d7c60ccec49e7c8b82a7700a93a5e2312e038
Onelio Viera <ojvwebdesign@gmail.com>
2024-07-11 16:09:15 -0600

    refactor: Update Navbar component with LinksDropdown and ThemeToggle

--- components/LinksDropdown.tsx
+++ components/LinksDropdown.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import {
+  DropdownMenu, 
+  DropdownMenuTrigger, 
+  DropdownMenuContent, 
+  DropdownMenuItem } from './ui/dropdown-menu'
+import { Button } from './ui/button'
+import { AlignLeft } from 'lucide-react'
+import Link from 'next/link'
+import links from '@/utils/links'
 
 const LinksDropDown = () => {
   return (
-    <div>
-      LinksDropdown
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className='lg:hidden'>
+        <Button
+          variant='outline'
+          size='icon'>
+          <AlignLeft />
+          <span className='sr-only'>Toggle Links</span>
+          </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-52 lg:hidden'
+        align='start'
+        sideOffset={25}>
+        {links.map((link) => <DropdownMenuItem key={link.href}>
+          <Link href={link.href}>
+            {link.icon} <span className='capitalize'>{link.label}</span>
+          </Link>
+        </DropdownMenuItem>)}
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
-export default LinksDropDown
+export default LinksDropDown;
